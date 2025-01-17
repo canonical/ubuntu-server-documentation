44d7130ab59dd0f39e49ec3009c1b157236acf77
Damilola Oladele <98895460+activus-d@users.noreply.github.com>
2025-01-17 10:23:22 +0100

    Create an issue template for the "Give feedback" button (#116)
    * create an issue template for the Give feedback button
    
    * improve issue-template yml file to solve validation errors
    
    * improve instruction

--- .sphinx/_static/github_issue_links.js
+++ .sphinx/_static/github_issue_links.js
@@ -11,18 +11,8 @@ window.onload = function() {
     link.classList.add("muted-link");
     link.classList.add("github-issue-link");
     link.text = "Give feedback";
-    link.href = (
-        github_url
-        + "/issues/new?"
-        + "title=docs%3A+TYPE+YOUR+QUESTION+HERE"
-        + "&body=*Please describe the question or issue you're facing with "
-        + `"${document.title}"`
-        + ".*"
-        + "%0A%0A%0A%0A%0A"
-        + "---"
-        + "%0A"
-        + `*Reported+from%3A+${location.href}*`
-    );
+    // use the issue template in the .github/ISSUE_TEMPLATE/feedback.yml file
+    link.href = github_url + "/issues/new?template=feedback.yml";
     link.target = "_blank";
 
     const div = document.createElement("div");
