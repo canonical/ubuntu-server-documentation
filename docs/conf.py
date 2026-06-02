857b0b345178be1da402d3ee6645755a578a0fee
Sally <sally.makin@canonical.com>
2026-06-02 18:15:52 +0100

    Update starter pack version (#722)
    * Move template to instructions file
    
    * Move files to match new sphinx stack structure
    
    * Remove/rename old .sphinx folder
    
    * Remove build artifacts
    
    * Update .gitignore
    
    * Update requirements file
    
    * Update Makefile
    
    * Update conf.py
    
    * Add .venv to ignored paths
    
    * update to new cookie banner

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,19 @@
 import datetime
 import os
+import textwrap
 import requests
 import sys
 import yaml
 
-# Configuration for the Sphinx documentation builder.
-# All configuration specific to your project should be done in this file.
+"""
+Configuration for the Sphinx documentation builder.
+All configuration specific to your project should be done in this file.
 
-# A complete list of built-in Sphinx configuration values:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-#
-# Our starter pack uses the custom Canonical Sphinx extension
-# to keep all documentation based on it consistent and on brand:
-# https://github.com/canonical/canonical-sphinx
+A complete list of built-in Sphinx configuration values:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+The Sphinx Stack uses the Canonical Sphinx theme to keep all documentation consistent and on brand: https://github.com/canonical/canonical-sphinx
+"""
 
 # ==============================================================================
 # Project information
@@ -21,32 +21,18 @@ import yaml
 
 # Project name
 project = "Ubuntu Server"
-author = "Canonical Group Ltd."
 
+# Author name; used in the default copyright statement in the page footer
+author = "Canonical Ltd."
 
-# Sidebar documentation title; best kept reasonably short
-# To include a version number, add it here (hardcoded or automated).
-# To disable the title, set to an empty string.
+# The year in the copyright statement
+copyright = f"{datetime.date.today().year}"
+
+# Sidebar documentation title
+# To disable the title, set it to an empty string.
 html_title = project + " documentation"
 
-
-# Copyright string; shown at the bottom of the page
-# NOTE: For static works, it is common to provide the first publication year.
-# Another option is to give the first year and current year for docs that change
-# frequently e.g. 2022–2023.
-#
-# A way to check a repo's creation date is to get a classic GitHub token with
-# "repo" permissions; see https://github.com/settings/tokens
-# Next, use "curl" and "jq" to extract the date from the API's output:
-#     curl -H 'Authorization: token <TOKEN>' \
-#      -H 'Accept: application/vnd.github.v3.raw' \
-#      https://api.github.com/repos/canonical/<REPO> | jq '.created_at'
-copyright = "%s, %s" % (datetime.date.today().year, author)
-
-
 # Documentation website URL
-# NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
-#       and is used by social media platforms; see https://ogp.me/
 ogp_site_url = "https://ubuntu.com/server/docs/"
 
 # Preview name of the documentation website
@@ -59,8 +45,7 @@ ogp_image = "https://assets.ubuntu.com/v1/cc828679-docs_illustration.svg"
 ogp_enable_meta_description = True
 
 # Product favicon; shown in bookmarks, browser tabs, etc.
-html_favicon = ".sphinx/_static/favicon.png"
-
+# html_favicon = ".sphinx/_static/favicon.png"
 
 # Dictionary of values to pass into the Sphinx context for all pages:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_context
@@ -69,22 +54,19 @@ html_context = {
     "product_page": "ubuntu.com/server",
 
     # Product tag image; the orange part of your logo, shown in the page header
-    "product_tag": "_static/tag.png",
+    # "product_tag": "_static/tag.png",
 
     # Your Discourse instance URL
-    # NOTE: If set, adding ":discourse: 123" to an .rst file will add a link to
-    # Discourse topic 123 at the bottom of the page.
     "discourse": "https://discourse.ubuntu.com/c/server/17",
 
     # Your Mattermost channel URL
-    "mattermost": "",
+    #"mattermost": "",
 
     # Your Matrix channel URL
     "matrix": "https://matrix.to/#/#server:ubuntu.com",
     
     # Your documentation GitHub repository URL
-    # NOTE: If set, links for viewing the docs source files and creating GitHub
-    # issues are added at the bottom of each page.
+    # If set, links for viewing the docs source files and creating GitHub issues are added at the bottom of each page.
     "github_url": "https://github.com/canonical/ubuntu-server-documentation/",
 
     # Docs branch in the repo; used in links for viewing the source files
@@ -93,15 +75,28 @@ html_context = {
     # Docs location in the repo; used in links for viewing the source files
     "repo_folder": "docs/",
 
-    # Required for feedback button    
-    "github_issues": "enabled",
-
     # To enable/disable the Previous/Next buttons at the bottom of pages.
     # Valid options: none, prev, next, both
     "sequential_nav": "none",
 
     # To enable listing contributors on individual pages, set to True
     "display_contributors": True,
+
+    # Required for feedback button    
+    "github_issues": "enabled",
+
+    # Passes the top-level 'author' value to the theme
+    "author": author,
+
+    # Documentation license information
+    "license": {
+        # TODO: Specify your project's license.
+        # For the name, we recommend using the standard shorthand identifier from
+        # https://spdx.org/licenses
+        "name": "",
+        # TODO: Link directly to your project's license statement.
+        "url": "",
+    },
 
     # Links for the "Ubuntu docs" dropdown in the site header
     "ubuntu_docs": [
@@ -116,27 +111,24 @@ html_context = {
     ],
 }
 
-# Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
-# If your documentation is hosted on https://docs.ubuntu.com/, specify the
-# project slug here.
-slug = "server/docs"
 
-
-# Allow opt-in build of the OpenAPI "Hello" example so docs stay clean by default.
-#if os.getenv("OPENAPI", ""):
-#    tags.add("openapi")
-#    html_extra_path.append("how-to/assets/openapi.yaml")
-
-
-# To enable the edit button on pages, add the link to a public repository.
+# TODO: To enable the edit button on pages, uncomment and change the link to a
+# public repository on GitHub or Launchpad. Any of the following link domains
+# are accepted:
+# - https://github.com/example-org/example"
+# - https://launchpad.net/example
+# - https://git.launchpad.net/example
 html_theme_options = {
     "source_edit_link": "https://github.com/canonical/ubuntu-server-documentation",
 }
 
 
-# By default, the documentation includes a feedback button at the top.
-# You can disable it by setting the following configuration to True.
-# We disable it because we have a custom feedback button with our issue template.
+# Project slug
+# If your documentation is hosted on https://docs.ubuntu.com/, specify the project slug here.
+slug = "server/docs"
+
+# By default, the documentation includes a feedback button at the top. 
+# We disable it because we have a custom feedback button that works with our issue template.
 disable_feedback_button = True
 
 
@@ -170,35 +162,9 @@ sitemap_filename = "doc-sitemap.xml"
 # Template and asset locations
 # ==============================================================================
 
-html_static_path = [".sphinx/_static/"]
+html_static_path = ["_static"]
 
-templates_path = [".sphinx/_templates/"]
-
-#html_extra_path = []
-
-# Add custom CSS files (located under "html_static_path")
-html_css_files = [
-    "css/cookie-banner.css",
-    "css/custom.css",
-    "css/footer.css",
-    "css/furo_colors.css",
-    "css/github_issue_links.css",
-    "css/header.css",
-]
-
-# Add custom JavaScript files (located under "html_static_path")
-html_js_files = [
-    "js/footer.js",
-    "js/github_issue_links.js",
-    "js/bundle.js",
-    "js/url_overwrite.js",
-]
-
-source_suffix = {
-#    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
-
+templates_path = ["_templates"]
 
 # ==============================================================================
 # Redirects
@@ -208,18 +174,36 @@ source_suffix = {
 # https://documatt.gitlab.io/sphinx-reredirects/usage.html
 # For example: "explanation/old-name.html": "../how-to/prettify.html",
 
-# NOTE: If undefined, set to None, or empty, the sphinx_reredirects extension
-# will be disabled.
+# NOTE: If undefined, set to None, or empty, the sphinx_reredirects extension will be disabled.
 redirects = {
     "how-to/containers/lxc-containers": "https://linuxcontainers.org/lxc/documentation/",
     "reference/backups/basic-backup-shell-script": "https://discourse.ubuntu.com/t/basic-backup-shell-script/36419"
 }
 
-# To set up internal redirects when a page has moved or been renamed, use
-# the redirects.txt file
+# To set up internal redirects when a page has moved or been renamed, use the redirects.txt file
 rediraffe_branch = "main"
 rediraffe_redirects = "redirects.txt"
 
+# Strips '/index.html' from destination URLs when building with 'dirhtml'
+rediraffe_dir_only = True
+
+
+# ==============================================================================
+# sphinx-llm configuration
+# ==============================================================================
+
+# This description is included in llms.txt to provide some initial context for your docs.
+# TODO: Add a description in the form "This is the documentation for <product name>,
+# <first sentence of home page>".
+llms_txt_description = textwrap.dedent(
+    """\
+    This is the official documentation for Ubuntu Server, a version of the Ubuntu operating system designed and engineered as a backbone for the internet.
+    """
+)
+
+# The base URL for references built by sphinx-markdown-builder.
+if os.environ.get("READTHEDOCS"):
+    markdown_http_base = html_baseurl
 
 # ==============================================================================
 # Link checker exceptions
@@ -300,13 +284,6 @@ manpages_url = (
 )
 
 
-# ==============================================================================
-# MyST configuration extras
-# ==============================================================================
-
-# Custom Sphinx extensions; see
-# https://www.sphinx-doc.org/en/master/usage/extensions/index.html
-
 # Custom MyST syntax extensions; see
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
 
@@ -323,21 +300,8 @@ myst_enable_extensions = {
     "html_admonition",
 }
 
-
-# NOTE: The canonical_sphinx extension is required for the starter pack.
-# It automatically enables the following extensions:
-# - custom-rst-roles
-# - myst_parser
-# - notfound.extension
-# - related-links
-# - sphinx_copybutton
-# - sphinx_design
-# - sphinx_reredirects
-# - sphinx_tabs.tabs
-# - sphinxcontrib.jquery
-# - sphinxext.opengraph
-# - terminal-output
-# - youtube-links
+# Custom Sphinx extensions; see
+# https://www.sphinx-doc.org/en/master/usage/extensions/index.html
 
 extensions = [
 # Included in the Starter Pack
@@ -352,6 +316,7 @@ extensions = [
     "sphinx_config_options",
     "sphinx_contributor_listing",
     "sphinx_filtered_toctree",
+    "sphinx_llm.txt",
     "sphinx_related_links",
     "sphinx_roles",
     "sphinx_terminal",
@@ -369,13 +334,48 @@ extensions = [
     "sphinxext.rediraffe",
     "sphinxcontrib.mermaid",
     "sphinx_copybutton",
-    "sphinx_llm.txt",
 ]
 
+# Excludes files or directories from processing
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    ".sphinx",
+    "cheatsheets/",
+    "doc-templates/*",
+    "doc-cheat-sheet*",
+    "readme.md",
+    "legacy/*.md",
+    ".github/*.md",
+    ".venv",
+]
 
 myst_heading_anchors = 3
 
+#html_extra_path = []
 
+# Adds custom CSS files, located remotely or in 'html_static_path'.
+html_css_files = [
+    "https://assets.ubuntu.com/v1/d86746ef-cookie_banner.css",
+    "css/custom.css",
+    "css/footer.css",
+    "css/furo_colors.css",
+    "css/github_issue_links.css",
+    "css/header.css",
+]
+
+# Adds custom JavaScript files, located remotely or in 'html_static_path'.
+html_js_files = [
+    "js/footer.js",
+    "js/github_issue_links.js",
+    "https://assets.ubuntu.com/v1/287a5e8f-bundle.js",
+    "js/url_overwrite.js",
+]
+
+source_suffix = {
+    ".md": "markdown",
+}
 # Suppress specific warnings
 suppress_warnings = [
     'myst.glossary',  # Suppress glossary-related warnings from MyST parser
@@ -395,22 +395,6 @@ extlinks = {
     "lpbug": ("https://bugs.launchpad.net/bugs/%s", "LP: #%s"),
     "matrix": ("https://matrix.to/#/#%s:ubuntu.com", "#%s:ubuntu.com"),
 }
-
-
-# Excludes files or directories from processing
-exclude_patterns = [
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
-    ".sphinx",
-    "cheatsheets/",
-    "doc-templates/*",
-    "doc-cheat-sheet*",
-    "readme.md",
-    "legacy/*.md",
-    ".github/*.md",
-]
-
 
 # Add tags that you want to use for conditional inclusion of text
 # (https://www.sphinx-doc.org/en/master/usage/restructuredtext/directives.html#tags)
